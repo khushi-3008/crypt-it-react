e8bd755b1c2f975f39e7a4afa517b8888c6f85d6
khushi-3008 <khushicarpenter@gmail.com>
2021-03-11 23:15:38 +0530

    Dashboard modified

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,48 @@
-import react from 'react';
-import Navigation from './Navigation';
-import Sidebar from './Sidebar';
 import './dashboard.css';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import { Redirect } from 'react-router';
 
 function Dashboard(){
+    let text = '<p>home</p>'
     return(
         <>
-        <Navigation/>
-        <Sidebar/>
+        <SideNav
+            onSelect ={(selected) => {
+                console.log(selected);
+        }}
+        >
+        <SideNav.Toggle />
+        <SideNav.Nav defaultSelected="home">
+            <NavItem eventKey="home">
+                <NavIcon>
+                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                </NavIcon>     
+                <NavText>
+                    Home
+                </NavText>
+                </NavItem>
+                <NavItem eventKey="charts">
+                <NavIcon>
+                    <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                </NavIcon>
+                <NavText>
+                    Charts
+                </NavText>
+                <NavItem eventKey="charts/linechart">
+                    <NavText>
+                        Line Chart
+                    </NavText>
+                </NavItem>
+                    <NavItem eventKey="charts/barchart">
+                        <NavText>
+                            Bar Chart
+                        </NavText>
+                    </NavItem>
+                </NavItem>    
+            </SideNav.Nav>
+        </SideNav>
         </>
     )
 }
